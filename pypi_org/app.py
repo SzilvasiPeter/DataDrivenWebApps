d1a6c198bbba32c552ec1ca24af7d720a29773f3
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-10-04 14:33:58 +0200

    Create layout template. Home templates extend it. Render about page in view-model.

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -17,5 +17,10 @@ def index():
     return flask.render_template('home/index.html', packages=test_packages)
 
 
+@app.route('/about')
+def about():
+    return flask.render_template('home/about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
