dae8f672472dca05efc5f2151c7e2d9c976d9c22
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-10-04 15:45:47 +0200

    Create a cms view with fake data

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -6,9 +6,11 @@ app = flask.Flask(__name__)
 def register_blueprint():
     from pypi_org.views import home_views
     from pypi_org.views import package_views
+    from pypi_org.views import cms_views
 
     app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(cms_views.blueprint)
 
 
 def main():
