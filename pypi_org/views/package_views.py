9aa20229c9878abd4d785af14677664e91cfe872
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-10-04 15:22:31 +0200

    Add new route with integer parameter

--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -11,3 +11,8 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 def package_details(package_name: str):
     return "Packages details for {}".format(package_name)
     # return flask.render_template('home/index.html', packages=test_packages)
+
+
+@blueprint.route('/<int:rank>')
+def popular(rank: int):
+    return "The details for the {}th most popular package".format(rank)
