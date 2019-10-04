96e1332f74a48383a2166f817590dee0eece80a0
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-10-04 15:20:37 +0200

    Refactor view-model. Create blueprint for each view. Register blueprint in the main app.

---
+++ pypi_org/views/package_views.py
@@ -0,0 +1,13 @@
+import flask
+
+from infrastructure.view_modifiers import response
+import services.package_service as package_service
+
+blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
+
+
+@blueprint.route('/project/<package_name>')
+# @response(template_file='packages/details.html')
+def package_details(package_name: str):
+    return "Packages details for {}".format(package_name)
+    # return flask.render_template('home/index.html', packages=test_packages)
