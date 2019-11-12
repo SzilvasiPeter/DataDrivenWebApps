7be257eb5a61825d2816773379862cdf0de5b278
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-11-12 20:18:17 +0100

    Using view model

--- pypi_org/viewmodels/shared/viewmodelbase.py
+++ pypi_org/viewmodels/shared/viewmodelbase.py
@@ -14,5 +14,6 @@ class ViewModelBase:
         self.error: Optional[str] = None
         self.user_id: Optional[int] = cookie_auth.get_user_id_via_auth_cookie(self.request)
 
-        def to_dict(self):
-            return self.__dict__
+    def to_dict(self):
+        return self.__dict__
+
