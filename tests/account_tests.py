99ac5816b13ffe2cbbdc5748e8e4e5ce08ddddaf
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-11-14 21:05:44 +0100

    Testing the register view model

--- tests/account_tests.py
+++ tests/account_tests.py
@@ -1,4 +1,29 @@
+from pypi_org.viewmodels.account.register_view_model import RegisterViewModel
+from tests.test_client import flask_app
+
 
 def test_example():
     print("Test example...")
     assert 1 + 2 == 3
+
+
+def test_register_validation_when_valid():
+    # 3 A's of testing: Arrange, Act, then Assert
+
+    # Arrange
+
+    form_data = {
+        'name': 'Peter Szilvasi',
+        'email': 'peti.szilvasi@gmail.com',
+        'password': 'abc123'
+    }
+
+
+    with flask_app.test_request_context(path='/account/register', data=form_data):
+        vm = RegisterViewModel()
+
+    # Act
+    vm.validate()
+
+    # Assert
+    assert vm.error is None
