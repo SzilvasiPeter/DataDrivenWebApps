ba34e4b9153e597a57994ae5548b753769d002b3
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-11-21 22:11:14 +0100

    Rewriting the our services using mongodb

--- pypi_org/services/user_service.py
+++ pypi_org/services/user_service.py
@@ -1,18 +1,15 @@
-import pypi_org.data.db_session as db_session
-from pypi_org.data.users import User
-
 from typing import Optional
+
 from passlib.handlers.sha2_crypt import sha512_crypt as crypto
+from pypi_org.nosql.users import User
 
 
 def get_user_count() -> int:
-    session = db_session.create_session()
-    return session.query(User).count()
+    return User.objects().count()
 
 
 def find_user_by_email(email: str) -> Optional[User]:
-    session = db_session.create_session()
-    return session.query(User).filter(User.email == email).first()
+    return User.objects().filter(email=email).first()
 
 
 def create_user(name: str, email: str, password: str) -> Optional[User]:
@@ -20,13 +17,11 @@ def create_user(name: str, email: str, password: str) -> Optional[User]:
         return None
 
     user = User()
-    user.name = name
     user.email = email
+    user.name = name
     user.hashed_password = hash_text(password)
 
-    session = db_session.create_session()
-    session.add(user)
-    session.commit()
+    user.save()
 
     return user
 
@@ -41,9 +36,7 @@ def verify_hash(hashed_text: str, plain_text: str) -> bool:
 
 
 def login_user(email: str, password: str) -> Optional[User]:
-    session = db_session.create_session()
-
-    user = session.query(User).filter(User.email == email).first()
+    user = find_user_by_email(email)
     if not user:
         return None
 
@@ -54,6 +47,5 @@ def login_user(email: str, password: str) -> Optional[User]:
 
 
 def find_user_by_id(user_id: int) -> Optional[User]:
-    session = db_session.create_session()
-    user = session.query(User).filter(User.id == user_id).first()
+    user = User.objects().filter(id=user_id).first()
     return user
